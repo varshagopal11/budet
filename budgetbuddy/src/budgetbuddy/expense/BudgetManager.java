1fe90f1b624ee4246606f382856aa8a5e1286975
acormond <acormond@ncsu.edu>
2019-03-02 13:28:03 -0500

    Add to BudgetManager

--- budgetbuddy/src/budgetbuddy/expense/BudgetManager.java
+++ budgetbuddy/src/budgetbuddy/expense/BudgetManager.java
@@ -2,6 +2,47 @@ package budgetbuddy.expense;
 
 public class BudgetManager {
 
-	BudgetManager instance = new BudgetManager();
+	private static BudgetManager instance = new BudgetManager();
+
+	private Income income;
+
+	private Food food;
+
+	private Gas gas;
+
+	private Insurance insurance;
+
+	private Rent rent;
+
+	private Utilities utilities;
+
+	public BudgetManager() {
+		this(null, null, null, null, null, null);
+	}
+
+	public BudgetManager(Income i, Food f, Gas g, Insurance ins, Rent r, Utilities u) {
+		income = i;
+		food = f;
+		gas = g;
+		insurance = ins;
+		rent = r;
+		utilities = u;
+	}
 	
+	public boolean isOver(AbstractExpense e) {
+		if (e instanceof Food) {
+			
+		}
+		return false;
+	}
+
+	public static BudgetManager getInstance() {
+		if (instance == null) {
+			instance = new BudgetManager();
+		}
+		return instance;
+	}
+	
+	
+
 }
